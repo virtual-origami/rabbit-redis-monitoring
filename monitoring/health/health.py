bf2fd38d49fd98c0a7c7cf4fb3d385a0a9613069
karsUniBremen <she@biba.uni-bremen.de>
2021-12-08 14:00:51 +0100

    Add health check tcp server

---
+++ monitoring/health/health.py
@@ -0,0 +1,37 @@
+import asyncio, socket
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
+                    format='%(levelname)-8s [%(filename)s:%(lineno)d] %(message)s')
+logger = logging.getLogger("metric monitor:Model")
+
+
+class HealthServer:
+    def __init__(self, config, event_loop):
+        self.host = ""
+        self.port = int(config["port"])
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.host, self.port))
+        self.server.listen(8)
+        self.server.setblocking(False)
+        self.event_loop = event_loop
+
+    @staticmethod
+    async def handle_client(client):
+        loop = asyncio.get_event_loop()
+        response = 'OK\n'
+        await loop.sock_sendall(client, response.encode('utf8'))
+        logger.debug("sent health status")
+        client.close()
+
+    async def server_loop(self):
+        try:
+            while True:
+                self.event_loop = asyncio.get_event_loop()
+                client, _ = await self.event_loop.sock_accept(self.server)
+                self.event_loop.create_task(self.handle_client(client))
+        except Exception as e:
+            return
+
+
